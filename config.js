9a403df54e79efb15e36c4fc5b345973511a0736
WangNgan <100012129+WangNgan@users.noreply.github.com>
2022-02-19 13:08:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,20 @@
 const CONFIG = {
-    titleWeb: "Ey yo, chào mừng đến zới thế zới nhảm nhí của WN",
+    titleWeb: "I want to say something with u",
     introTitle: 'Hi Thu Chemm',
-    introDesc:
-    `Mưa ngấm vào đất
-    Ngấm ngầm cho cây
-    Mình cũng ngất ngây
-    Ngấm vào ai đấy.`,
+    introDesc: `Mưa ngấm vào đất`
+    `Ngấm ngầm cho cây`
+    `Mình cũng ngất ngây`
+    `Ngấm vào ai đấy`,
     btnIntro: ':)) Thì là zậy đó',
-    title: 'Dẫu anh không là chàng hoàng tử mà em đắm say',
-    desc: 'Chỉ là giản đơn thôi như hình hài em lúc này, anh vẫn muốn quỳ gối trước nàng công chúa đẹp nhất đêm nay ',
+    title: 'Dẫu anh không là chàng hoàng tử mà em đắm say', 
+            'Chỉ đơn giản thôi như hình hài anh lúc này',
+    desc: 'Anh vẫn muốn quỳ gối trước nàng công chúa, đẹp nhất đêm nay 🥺',
     btnYes: 'Gkee zậy sao',
     btnNo: 'Xàm quá :)))',
-    question: 'Liệu chúng ta có thể bắt đầu một mối quan hệ mới, như là người yêu chẳng hạn? :v',
-    btnReply: 'Cho anh biết nhé!',
-    reply: 'ỏhhh, được thôi ạ :> ',
-    mess: 'em như mùa hạ, anh tựa tình ca, tình ca mùa hạ, là dành cho ta ❤😎',
+    question: 'Anh có câu hỏi là: liệu chúng ta có thể bắt đầu một mối quan hệ mới, như là người yêu chẳng hạn? :v',
+    btnReply: 'Gửi ở đây nè!',
+    reply: 'ỏhhh, đáng iu quá zị nè 😻 ',
+    mess: 'Socola thì ngọt; Còn em thì ngọt hơn; Nếu em cũng có ý; Thì mình hết cô đơn. ❤😎',
     messDesc: 'Xin phép được chở em đi học từ thứ hai nhé :> ',
     btnAccept: 'Dạ zângg',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: 'https://www.facebook.com/messages/t/100020801720912' //link mess của các bạn. VD: https://m.me/nam.nodemy
