f4bea6b6be6d8634ea3fd62c1ea8e249fe63a74d
WangNgan <100012129+WangNgan@users.noreply.github.com>
2022-02-19 12:35:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên của bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    titleWeb: "Ey yo, chào mừng đến zới thế zới nhảm nhí của WN",
+    introTitle: 'Hi Thu Chemm',
+    introDesc: `Mưa ngấm vào đất
+    Ngấm ngầm cho cây
+    Mình cũng ngất ngây
+    Ngấm vào ai đấy.`,
+    btnIntro: ':)) Thì là zậy đó',
+    title: 'Dẫu anh không là chàng hoàng tử mà em đắm say',
+    desc: 'Chỉ là giản đơn thôi như hình hài em lúc này, anh vẫn muốn quỳ gối trước nàng công chúa đẹp nhất đêm nay ',
+    btnYes: 'Gkee zậy sao',
+    btnNo: 'Xàm quá :)))',
+    question: 'Liệu anh có thể trở thành người sẽ chở em đi học mỗi ngày được hong :v',
+    btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Xin phép được chở em từ thứ hai nhé :> ',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
